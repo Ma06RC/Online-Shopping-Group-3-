6b0e9f3cc118312712ede2ce0fb3ccd8fcf294a6
cuanlategan <cuanlategan@gmail.com>
2016-06-26 13:53:03 +1200

    FB login works, need refinements

--- app.js
+++ app.js
@@ -9,63 +9,12 @@ var clientSessions = require("client-sessions");
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var listings = require('./routes/listings');
+var facebookLogin = require('./routes/facebookOAuth');
 
-var passport = require('passport');
-var Strategy = require('passport-facebook').Strategy;
-var models = require('./models');
 
 var app = express();
 
-passport.use(new Strategy({
-      clientID: '496438923889701',
-      //process.env.CLIENT_ID,
-      clientSecret: 'b007bf66831f20c35bce0099c16784e3',
-      //process.env.CLIENT_SECRET,
-      callbackURL: 'https://still-ocean-25340.herokuapp.com/login/facebook/return',//'http://localhost:3000/login/facebook/return'//
-      profileFields: ['name','emails']
-    },
-    function(accessToken, refreshToken, profile, cb) {
-        console.log("in app.js - accessToken ", accessToken);
-        console.log("in app.js - refreshToken ", refreshToken);
-        console.log("in app.js - profile ", profile);
-        console.log("in app.js - profileID ", profile.id);
-      models.User.findOrCreate({ where:{username: profile.id } ,defaults: {password: 'FACEBOOK'}}).then( function(results){
-          console.log("in apps.js - results ", results);
 
-          return cb(null, profile)
-
-      })
-    }));
-
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
-
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Define routes //.
-app.get('/login/facebook', passport.authenticate('facebook',{scope: 'email'}));
-
-app.get('/login/facebook/return',
-    passport.authenticate('facebook', { failureRedirect: '/loginFail' }),
-
-    function(req, res) {
-        console.log("facebook return success");
-      res.redirect('/');
-    });
-
-app.get('/profile',
-    require('connect-ensure-login').ensureLoggedIn(),
-    function(req, res){
-      res.render('profile', { user: req.user });
-    });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -85,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/listings', listings);
+app.use('/login/facebook', facebookLogin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/facebookOAuth.js
@@ -0,0 +1,61 @@
+var models = require('../models');
+var express = require('express');
+var router = express.Router();
+
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
+var models = require('./models');
+
+
+passport.use(new Strategy({
+        clientID: '496438923889701',
+        //process.env.CLIENT_ID,
+        clientSecret: 'b007bf66831f20c35bce0099c16784e3',
+        //process.env.CLIENT_SECRET,
+        callbackURL: 'https://still-ocean-25340.herokuapp.com/login/facebook/return',//'http://localhost:3000/login/facebook/return'//
+        profileFields: ['name','emails']
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        console.log("in app.js - accessToken ", accessToken);
+        console.log("in app.js - refreshToken ", refreshToken);
+        console.log("in app.js - profile ", profile);
+        console.log("in app.js - profileID ", profile.id);
+        models.User.findOrCreate({ where:{username: profile.id } ,defaults: {password: 'FACEBOOK'}}).then( function(results){
+            console.log("in apps.js - results ", results);
+
+            return cb(null, profile)
+
+        })
+    }));
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+});
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define routes //.
+app.get('/login/facebook', passport.authenticate('facebook',{scope: 'email'}));
+
+app.get('/login/facebook/return',
+    passport.authenticate('facebook', { failureRedirect: '/loginFail' }),
+
+    function(req, res) {
+        console.log("facebook return success");
+        res.redirect('/');
+    });
+
+app.get('/profile',
+    require('connect-ensure-login').ensureLoggedIn(),
+    function(req, res){
+        res.render('profile', { user: req.user });
+    });
+
+module.exports = router;
