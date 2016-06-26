a046c0a08d5f8ba3ff4a89d2aa9e6031025f8a0b
Raygeuze <sk-8-rguy@hotmail.com>
2016-06-26 14:31:37 +1200

    trying timeout

--- routes/users.js
+++ routes/users.js
@@ -69,9 +69,9 @@ router.post('/login', function (req, res) {
         } else {
             req.session_state.username = user.username;
             //set the login time here
-            // var date = new date();
-            // req.session_state.loginTime = date.getMinutes();
-            // console.log("setting login time");
+            var date = new date();
+            req.session_state.loginTime = date.getMinutes();
+            console.log("setting login time");
             res.redirect('/');
         }
     });
