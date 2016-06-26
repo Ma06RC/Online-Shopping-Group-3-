a30ba72b27d4a4e8a0391ce2e6994ece02d0b95e
Raygeuze <sk-8-rguy@hotmail.com>
2016-06-26 15:33:25 +1200

    trying timout

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get('*',function(req,res,next){
       || date.getMinutes() < req.session_state.loginTime-1){
       //logout?
       req.session_state.reset();
+      res.redirect('/');
       console.log("logout");
     }
     else{
