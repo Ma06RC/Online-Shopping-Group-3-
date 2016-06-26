80630de75df3ddde634267de74c0d127ede57500
cuanlategan <cuanlategan@gmail.com>
2016-06-26 14:39:47 +1200

    FB login works, need refinements

--- app.js
+++ app.js
@@ -15,46 +15,6 @@ var facebookLogin = require('./routes/facebookOAuth');
 var app = express();
 
 
-<<<<<<< HEAD
-          return cb(null, profile)
-
-      })
-    }));
-
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
-
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Define routes.
-app.get('/login/facebook', passport.authenticate('facebook',{scope: 'email'}));
-
-app.get('/login/facebook/return',
-    passport.authenticate('facebook', { failureRedirect: '/loginFail' }),
-    
-    function(req, res) {
-        console.log("facebook return success");
-        res.set('Cache-Control', 'no-cache'); // Passport behaviour must be invoked.
-      res.redirect('/');
-    });
-
-app.get('/profile',
-    require('connect-ensure-login').ensureLoggedIn(),
-    function(req, res){
-      res.set('Cache-Control', 'no-cache'); // Passport behaviour must be invoked.
-      res.render('profile', { user: req.user });
-    });
-=======
->>>>>>> 749e0d5504f96ec7139935d5cd562fdee03a0042
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
